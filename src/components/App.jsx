a1e3e6d0b2701ec92bfe49c826d1d6fc124d3ef8
irina77676e <114076050+irina77676e@users.noreply.github.com>
2023-05-16 08:56:10 +0200

    change name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,7 +35,7 @@ class App extends Component {
     }
   };
 
-  deleteContact = contactId => {
+  removeContact = contactId => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
@@ -75,7 +75,7 @@ class App extends Component {
         <Filter value={this.state.filter} onChange={this.changeFilter} />
         <ContactList
           contacts={visibleContacts}
-          onRemoveContact={this.deleteContact}
+          onRemoveContact={this.removeContact}
         />
       </Div>
     );
